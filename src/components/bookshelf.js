b4df339c4b1ba96c29201049f28bbbe0d95fd33c
Irina Illustrova <illustrova@gmail.com>
2018-08-01 23:49:16 +0300

    fix: Handle invalid search query + minor fixes

--- src/components/bookshelf.js
+++ src/components/bookshelf.js
@@ -3,33 +3,33 @@ import Book from './book';
 import defaultCover from '../icons/defaultCover.svg';
 
 class Bookshelf extends Component {
-	render() {
-		const {heading, updateShelf} = this.props;
-		return (
-			<div className="bookshelf">
-				<h2 className="bookshelf-title">{heading}</h2>
-				<div className="bookshelf-books">
-					<ol className="books-grid">
-						{this.props.books.map(book => (
-							<Book
-								key={book.id}
-								id={book.id}
-								authors={book.authors}
-								title={book.title}
-								shelf={book.shelf}
-								thumbnail={
-									book.imageLinks && book.imageLinks.thumbnail
-										? book.imageLinks.thumbnail
-										: defaultCover
-								}
-								updateShelf={updateShelf}
-							/>
-						))}
-					</ol>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const {heading, updateShelf} = this.props;
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{heading}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map(book => (
+              <Book
+                key={book.id}
+                id={book.id}
+                authors={book.authors ? book.authors : ''}
+                title={book.title}
+                shelf={book.shelf}
+                thumbnail={
+                  book.imageLinks && book.imageLinks.thumbnail
+                    ? book.imageLinks.thumbnail
+                    : defaultCover
+                }
+                updateShelf={updateShelf}
+              />
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Bookshelf;
--- src/components/search.js
+++ src/components/search.js
@@ -5,81 +5,86 @@ import Book from './book';
 import defaultCover from '../icons/defaultCover.svg';
 
 class Search extends Component {
-	constructor() {
-		super();
-		this.state = {
-			query: '',
-			books: [],
-		};
-	}
+    constructor() {
+        super();
+        this.state = {
+            query: '',
+            books: [],
+        };
+    }
 
-	updateQuery = query => {
-		const {myBooks} = this.props;
+    updateQuery = query => {
+        const {myBooks} = this.props;
 
-		this.setState({query: query});
-		if (query.trim() === '') {
-			return;
-		}
-		BooksAPI.search(query.trim()).then(response => {
-			if (response && response.length) {
-				const books = response
-					.filter(book => {
-						return !myBooks.find(myBook => myBook.id === book.id);
-					})
-					.map(book => {
-						return {
-							id: book.id,
-							shelf: 'none',
-							authors: book.authors,
-							title: book.title,
-							thumbnail:
-								book.imageLinks && book.imageLinks.thumbnail
-									? book.imageLinks.thumbnail
-									: defaultCover,
-						};
-					});
-				this.setState({books});
-			}
-		});
-	};
+        this.setState({query: query});
+        if (query.trim() === '') {
+            this.setState({books: []})
+            return;
+        }
+        BooksAPI.search(query.trim()).then(response => {
+            if (response && response.length) {
+                const books = response
+                    .filter(book => {
+                        return !myBooks.find(myBook => myBook.id === book.id);
+                    })
+                    .map(book => {
+                        console.log('book.authors => ',book.authors);
+                        return {
+                            id: book.id,
+                            shelf: 'none',
+                            authors: book.authors ? book.authors :[],
+                            title: book.title,
+                            thumbnail:
+                                book.imageLinks && book.imageLinks.thumbnail
+                                    ? book.imageLinks.thumbnail
+                                    : defaultCover,
+                        };
+                    });
+                this.setState({books});
+            }
+            else {
+                this.setState({books: []})
+            }
+        });
+    };
 
-	render() {
-		const {books} = this.state;
-		const {updateShelf} = this.props;
+    render() {
+        const {books} = this.state;
+        const {updateShelf} = this.props;
 
-		return (
-			<div className="search-books">
-				<div className="search-books-bar">
-					<Link to="/" className="close-search">
-						Close
-					</Link>
-					<div className="search-books-input-wrapper">
-						<input
-							type="text"
-							placeholder="Search by title or author"
-							onChange={event =>
-								this.updateQuery(event.target.value)
-							}
-						/>
-					</div>
-				</div>
-				<div className="search-books-results">
-					<ol className="books-grid">
-						{books.map(book => (
-							<Book
-								key={book.id}
-								id={book.id}
-								authors={book.authors}
-								title={book.title}
-								thumbnail={book.thumbnail}
-								updateShelf={updateShelf}
-							/>
-						))}
-					</ol>
-				</div>
-			</div>
-		);
-	}
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to="/" className="close-search">
+                        Close
+                    </Link>
+                    <div className="search-books-input-wrapper">
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            onChange={event =>
+                                this.updateQuery(event.target.value)
+                            }
+                        />
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {books.map(book => (
+                            <Book
+                                key={book.id}
+                                id={book.id}
+                                authors={book.authors.join(', ')}
+                                title={book.title}
+                                thumbnail={book.thumbnail}
+                                updateShelf={updateShelf}
+                            />
+                        ))}
+                    </ol>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Search;
